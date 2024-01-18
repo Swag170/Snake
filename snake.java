daff1ed135bbbed57f025e32fdd7d0734b1a6b3d
kolbycyra <156241556+kolbycyra@users.noreply.github.com>
2024-01-17 17:19:49 -0800

    Print board method snake.java

--- snake.java
+++ snake.java
@@ -1,8 +1,13 @@
 public class Snake{
-
 	public static void main(String[] args){
-
-		System.out.println("Hello, world!");//jazz
-	
+		//Simple array print method
+   	 	public static void printBoard(char[][] arr){
+        		for(int row = 0; row < arr.length; row++){
+            			for(int col = 0; col < arr[row].length; col++){
+               	 		System.out.print(arr[row][col] + " ");
+            			}
+            		System.out.println();
+        		}
+    		}	
 	}
 }
