14f20aaf48ab4461f92f07be2693ecf40ee5c22b
Swag170 <112878175+Swag170@users.noreply.github.com>
2024-01-19 12:23:32 -0800

    Create snakeObj.java
    added snaked object

---
+++ snakeObj.java
@@ -0,0 +1,12 @@
+public class snakeObj {     //stated coding at 1pm
+    int x;
+    int y;
+    String type = "B";
+    void addSnake(int Y, int X){
+        x=X;
+        y=Y;
+    }
+    void setSnakeType(String type){
+        type = this.type;
+    }
+}
