a9812c6efecb51976dacc95c396218828ded4d2b
kolbycyra <156241556+kolbycyra@users.noreply.github.com>
2024-01-24 17:09:37 -0800

    Updated move.java too add food spawn

--- move.java
+++ move.java
@@ -2,17 +2,25 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
+import java.util.Random;
 
 public class move extends JFrame implements KeyListener {
 
     private char[][] board;
+    private boolean foodSpawned = false;
     private int snakeX = 0;
     private int snakeY = 0;
+    private int foodX;
+    private int foodY;
+    int rows = 10;
+	int cols = 10;
 
     public move() {
-        board = new char[10][10];
+   
+        board = new char[rows][cols];
         makeBoard();
-
+        spawnFood();
+        
         JTextArea textArea = new JTextArea();
         textArea.setEditable(false);
         textArea.addKeyListener(this);
@@ -33,6 +41,20 @@ public class move extends JFrame implements KeyListener {
             }
         }
         board[snakeX][snakeY] = 'X';
+        board[foodX][foodY] = 'F';
+    }
+    private void spawnFood() {
+    	Random rand = new Random();
+    	while(true) {
+    		int randX = rand.nextInt(rows);
+        	int randY = rand.nextInt(cols);
+    		if (board[randX][randY] == '0') {
+    			board[randX][randY] = 'F';
+    			foodX = randX;
+    			foodY = randY;
+    			break;
+    		}
+    	}
     }
 
     private void displayBoard() {
@@ -52,7 +74,7 @@ public class move extends JFrame implements KeyListener {
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
         // Clear the console
-        System.out.print("\033[H\033[2J");
+        System.out.println("");
         switch (keyCode) {
             case 38:
                 if (snakeX > 0) {
@@ -74,6 +96,9 @@ public class move extends JFrame implements KeyListener {
                     snakeY++;
                 }
                 break;
+        }
+        if(foodX == snakeX && foodY == snakeY) { //When head of snake goes over the food, new food spawns
+        	spawnFood();
         }
         makeBoard();
         displayBoard();
